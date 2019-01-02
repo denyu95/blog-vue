f3bbbfe2c6d5b849cc1634b507aa47a2ee9c803c
mike <extrnight@126.com>
2019-01-02 18:33:55 +0800

    前后台打通

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 v-html="msg"></h1>
+    <button v-on:click="click">请求</button>
   </div>
 </template>
+
+<script>
+  import axios from 'axios';
+  // import Vue from 'vue';
+
+  export default {
+    name: 'home',
+    data: function () {
+      return {
+        msg: 0
+      }
+    },
+    methods: {
+      click(){
+        /* eslint-disable */
+        console.log('haha')
+        axios.get(`http://123.206.221.18:8081/mgt/article`).then(({ data }) => {
+          this.msg = data.data.content
+        });
+      }
+    }
+  }
+</script>
