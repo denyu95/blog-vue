e553ba10209f03e6bac32d0404350f848693f9c9
denyu95 <912078294@qq.com>
2019-01-03 17:11:14 +0800

    修改查询文章的接口地址

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,26 +1,27 @@
 <template>
   <div class="about">
     <h1 v-html="msg"></h1>
+    <input v-model="message">
     <button v-on:click="click">请求</button>
   </div>
 </template>
 
 <script>
   import axios from 'axios';
-  // import Vue from 'vue';
 
   export default {
     name: 'home',
     data: function () {
       return {
-        msg: 0
+        msg: 0,
+        message: ''
       }
     },
     methods: {
       click(){
         /* eslint-disable */
         console.log('haha')
-        axios.get(`http://123.206.221.18:8081/mgt/article`).then(({ data }) => {
+        axios.get(`http://123.206.221.18:8081/web/article/`+this.message).then(({ data }) => {
           this.msg = data.data.content
         });
       }
